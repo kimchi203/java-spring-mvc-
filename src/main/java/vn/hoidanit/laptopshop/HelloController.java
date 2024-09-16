56fdaa64bade052d187454090e8cc02ad3f37e33
kimchi203 <kimchi15062k3@gmail.com>
2024-09-16 17:28:38 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,15 @@ public class HelloController {
     public String index() {
         return "Hello World kim chi !";
     }
+    
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+    
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
+    }
 }
+ 
