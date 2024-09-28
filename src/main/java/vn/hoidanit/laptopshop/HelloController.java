edede8e00e0749b56cd1559715e0e66fb1e0cb78
kimchi203 <kimchi15062k3@gmail.com>
2024-09-28 14:38:42 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World kim chi !";
+        return "Hello World Chi !";
     }
     
     @GetMapping("/user")
